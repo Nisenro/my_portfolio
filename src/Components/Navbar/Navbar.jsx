a93f4e187fb91eac4f8aaa39ef253c41cd0343c8
Nisenro <adegbenroadeseke@gmail.com>
2024-05-16 23:39:12 +0100

    set up using vite and react

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Navbar.css';
+import logo from '../../assets/logo.svg';
+
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <img src={logo} alt="" />
+            <ul className='nav_menu'>
+                <li href="home">Home</li>
+                <li href="about">About Me</li>
+                <li href="experience">Experience</li>
+                <li href="skills">Skills</li>
+                <li href="contact">Contact</li>
+            </ul>
+            <div className="nav_connect">Connect With Me</div>
+        </div>
+    )
+}
+export default Navbar;
