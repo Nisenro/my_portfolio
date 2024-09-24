6fb5821c83bb52b8a9ff889ad338f3aa306221b7
Nisenro <adegbenroadeseke@gmail.com>
2024-09-24 11:02:39 +0100

    hero page ready

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './About.css'
+
+const About = () => {
+    return (
+        <div className='about'>
+            <div className="about_title">
+                <h1>About me</h1>
+            </div>
+            <div className="about_section">
+                <p className='pink'>I am a recent graduate with a Master's degree in Computing with an internship experience in software development where I honed my skills in front-end technologies such as HTML, CSS, and JavaScript, contributing to several successful projects.</p>
+                <p className='green'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae distinctio officia deserunt quia tempora harum earum deleniti dolorem assumenda libero? Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, qui.</p>
+                <p className='blue'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae distinctio officia deserunt quia tempora harum earum deleniti dolorem assumenda libero? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum, ab.</p>
+                <p className='yellow'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae distinctio officia deserunt quia tempora harum earum deleniti dolorem assumenda libero? Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, voluptate?</p>
+                <p className='purple'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae distinctio officia deserunt quia tempora harum earum deleniti dolorem assumenda libero? Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, voluptate?</p>
+            </div>
+        </div>
+
+    )
+}
+
+export default About
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,8 +6,8 @@ const Hero = () => {
     return (
         <div className='hero'>
             <div className="text_section">
-                <h2>I am <p className='name'>Adeseke Adegbenro,</p> a software developer based in the United Kingdom</h2>
-                <p>Entry-level Frontend Developer with a Master's in Computing and Fintech internship experience.</p>
+                <h2>I am <p className='name'>Adeseke Adegbenro,</p> a software developer based in the United Kingdom.</h2>
+                <p className='bio'>Entry-level Frontend Developer with a Master's in Computing and Fintech internship experience.</p>
                 <div className="hero_action">
                     <div className="hero_link">Link With Me</div>
                     <div className="hero_resume">My resume</div>
